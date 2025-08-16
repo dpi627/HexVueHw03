f3fbc7e04836292d65a280e3fdc378556e4d653e
brian <dpi.studio@gmail.com>
2025-08-16 21:21:13 +0800

    feat: 新增結帳視圖，顯示購物車項目及總價

---
+++ src/views/CheckoutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h2>Cart Items</h2>
+      <ul>
+        <li v-for="(item, index) in cart.items" :key="index">
+          {{ item.name }} - {{ item.price }}
+          <button @click="cart.removeItem(item)">Remove</button>
+        </li>
+      </ul>
+      <p>Total: {{ cart.totalPrice }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useCartStore } from '@/stores/cart'
+
+const cart = useCartStore()
+</script>
