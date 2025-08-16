ec8975406fbe48e2e2f7ac6bcafd8799c021a8b6
brian <dpi.studio@gmail.com>
2025-08-16 11:29:42 +0800

    feat: 先完成一個簡單範例

--- src/EazySample.vue
+++ src/EazySample.vue
@@ -1,24 +1,25 @@
 <template>
-  <header class="header">
-    <h1>Eazy Header</h1>
-  </header>
-  <div class="row">
-    <div class="col-4">
-      <div class="card">
-        <img
-          src="
-        https://images.unsplash.com/photo-1755217908514-785dbc5ad381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
-          class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
-            content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="container">
+    <header class="header">
+      <h1>Eazy Header</h1>
+    </header>
+    <div class="row">
+      <div class="col-4" v-for="i in 10" :key="i">
+        <div class="card">
+          <img
+            src="https://images.unsplash.com/photo-1755217908514-785dbc5ad381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+            class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
       </div>
     </div>
+    <footer class="footer">
+      Eazy Footer
+    </footer>
   </div>
-  <footer class="footer">
-    Eazy Footer
-  </footer>
 </template>
