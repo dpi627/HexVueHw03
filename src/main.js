d2890c1630eb141d2be6a3337935676015c35bfe
brian <dpi.studio@gmail.com>
2025-08-16 11:45:26 +0800

    fix: 修正 main.js 中的 App 引入路徑，並註解 HomeView.vue 中的 TheWelcome 組件

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './EazySample.vue'
+import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
