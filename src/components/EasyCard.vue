3d966104a0cf219d3e189e528462f5a405ecc1cb
brian <dpi.studio@gmail.com>
2025-08-16 16:24:35 +0800

    fix: 修正 EasyCard 組件中 emit 事件的名稱，並確保按鈕點擊事件正確阻止預設行為

--- src/components/EasyCard.vue
+++ src/components/EasyCard.vue
@@ -14,9 +14,11 @@
           </pre>
         </code>
       </p>
-      <a href="#" class="btn btn-primary" @click="changeOutterTitle">
+      <a href="#" class="btn btn-primary" @click.prevent="changeOutterTitle">
         Go somewhere
       </a>
+      <br />
+      <a href="#" @click.prevent="emit('emit-title', '另一個內層修改')">Click</a>
     </div>
   </div>
 </template>
@@ -27,11 +29,11 @@ import { defineProps, defineEmits } from 'vue';
 defineProps(['innerData', 'value']);
 
 // 定義事件名稱
-const emit = defineEmits(['emitTitle']);
+const emit = defineEmits(['emit-title']);
 
 // 定義方法
 const changeOutterTitle = () => {
   console.log('內層觸發 emit')
-  emit('emitTitle', '內層送出的修改')
+  emit('emit-title', '內層送出的修改')
 }
 </script>
