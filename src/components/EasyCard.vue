6d6cc9df28f3e4deea0d049b5d147570365296eb
brian <dpi.studio@gmail.com>
2025-08-16 12:37:52 +0800

    feat: 在 EasyCard 組件中新增計數器功能
    掩飾元件本身狀態獨立，變更參數不會影響到其他元件

--- src/components/EasyCard.vue
+++ src/components/EasyCard.vue
@@ -4,10 +4,18 @@
       src="https://images.unsplash.com/photo-1755217908514-785dbc5ad381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
       class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">Card title ({{ counter }})</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
         content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-primary" @click="counter++">
+        Go somewhere
+      </a>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const counter = ref(0)
+</script>
