1d306beb5e64b381529b747990cdaefbb4cb930e
brian <dpi.studio@gmail.com>
2025-08-16 12:07:22 +0800

    feat: 新增 EasyCard 和 EasyFooter 組件，並在 EasySampleView 中使用

---
+++ src/components/EasyCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <img
+      src="https://images.unsplash.com/photo-1755217908514-785dbc5ad381?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+      class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+        content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
