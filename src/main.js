48d839d500130265dbb28f95cdbf623e78106583
brian <dpi.studio@gmail.com>
2025-08-16 10:58:28 +0800

    feat: 更新 all.scss 檔案以覆蓋 Bootstrap 變數，並在 main.js 中調整 Bootstrap 引入順序

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/all.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
