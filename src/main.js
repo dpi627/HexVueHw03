7dc1262ece3c50324dc25e9cf1b584cd8394b02e
brian <dpi.studio@gmail.com>
2025-08-17 16:22:14 +0800

    feat: 更新應用程式主組件為 AppHw3.vue，並新增飲品列表與訂單功能

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from './AppHw3.vue'
 import router from './router'
 
 const app = createApp(App)
