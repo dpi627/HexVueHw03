9eecb1cdc8d8934ed8f8e96e070f2e93f9e3144d
brian <dpi.studio@gmail.com>
2025-08-18 13:31:08 +0800

    feat: 新增產品列表元件，整合飲品顯示功能

--- src/AppHw3.vue
+++ src/AppHw3.vue
@@ -3,15 +3,7 @@
     <div class="container mt-5">
       <div class="row">
         <div class="col-md-4">
-          <div class="list-group" v-for="drink in drinks" :key="drink.id">
-            <a href="#" @click.prevent="cart.addItem(drink)" class="list-group-item list-group-item-action">
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">{{ drink.name }}</h5>
-                <small>${{ drink.price }}</small>
-              </div>
-              <p class="mb-1">{{ drink.description }}</p>
-            </a>
-          </div>
+          <ProductList />
         </div>
         <div class="col-md-8">
           <template v-if="cart.totalCount">
@@ -111,6 +103,7 @@
 import { ref } from 'vue'
 import { useCartStore } from '@/stores/cart'
 import { useOrderStore } from '@/stores/order'
+import ProductList from './components/ProductList.vue'
 
 const cart = useCartStore()
 const order = useOrderStore()
@@ -130,55 +123,6 @@ const addMemoSample = (memo) => {
   cart.memo += (cart.memo) ? `, ${memo}` : memo
 }
 
-const drinks = ref([
-  {
-    "id": 1,
-    "name": "珍珠奶茶",
-    "description": "香濃奶茶搭配QQ珍珠",
-    "price": 50
-  },
-  {
-    "id": 2,
-    "name": "冬瓜檸檬",
-    "description": "清新冬瓜配上新鮮檸檬",
-    "price": 45
-  },
-  {
-    "id": 3,
-    "name": "翡翠檸檬",
-    "description": "綠茶與檸檬的完美結合",
-    "price": 55
-  },
-  {
-    "id": 4,
-    "name": "四季春茶",
-    "description": "香醇四季春茶，回甘無比",
-    "price": 45
-  },
-  {
-    "id": 5,
-    "name": "阿薩姆奶茶",
-    "description": "阿薩姆紅茶搭配香醇鮮奶",
-    "price": 50
-  },
-  {
-    "id": 6,
-    "name": "檸檬冰茶",
-    "description": "檸檬與冰茶的清新組合",
-    "price": 45
-  },
-  {
-    "id": 7,
-    "name": "芒果綠茶",
-    "description": "芒果與綠茶的獨特風味",
-    "price": 55
-  },
-  {
-    "id": 8,
-    "name": "抹茶拿鐵",
-    "description": "抹茶與鮮奶的絕配",
-    "price": 60
-  }
-])
+
 
 </script>
